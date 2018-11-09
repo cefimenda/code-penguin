49c9a6b7a83b69c3b78688f0272825f1745f9c80
sandynism <31261960+Sandynism@users.noreply.github.com>
2018-11-08 19:55:10 -0500

    makeshift solutions page for mvp

--- client/src/components/SolveBox/SolveBox.js
+++ client/src/components/SolveBox/SolveBox.js
@@ -1,22 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import './SolveBox.css';
 
 export default class SolveBox extends Component {
-    render() {
-        let padd = ""
-
-        // if (this.props.padding === "left") {
-        //     padd = "50px 100px 50px 580px" 
-        // } else if (this.props.padding === "right") {
-        //     padd = "50px 550px 50px 100px" 
-        // } else {
-        //     padd = "50px 100px" 
-        // }
-
-        return (
-            <div className="main-container" style={{ padding: `${padd}`, backgroundColor: `${this.props.bgcolor}`}}>
-                {this.props.children}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <div className="main-container">
+          <iframe
+            title="codesandbox"
+            className="codesandbox"
+            src="https://codesandbox.io/embed/new?codemirror=1"
+            style={{
+              width: '100%',
+              height: '500px',
+              marginTop: '10px',
+              border: '0',
+              borderRadius: '4px',
+              overflow: 'hidden'
+            }}
+            sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
+          />
+        </div>
+        {/* <textarea name="" id="" cols="90" rows="30"></textarea> */}
+        {/* <iframe width="100%" height="300" src="//jsfiddle.net/reactjs/69z2wepo/embedded/" allowfullscreen="allowfullscreen" allowpaymentrequest frameborder="0"></iframe> */}
+      </React.Fragment>
+    );
+  }
 }
