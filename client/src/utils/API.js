61488a8768a9a05a48c565852991afb475eb4fae
Evan Simon Ross <evan.simon.ross@gmail.com>
2018-11-10 13:02:12 -0500

    Added getUser in client side API functions

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export default {
+  getUser: function() {
+    return axios.post("http://localhost:4141/fn/tasks/getUser");
+  },
   getTasks: function() {
     return axios.post("http://localhost:4141/fn/tasks/readAllTasks");
   }
