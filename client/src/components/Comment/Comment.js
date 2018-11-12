1d7c092929730ab22e6abcd61ce005cc1933c07c
sandynism <31261960+Sandynism@users.noreply.github.com>
2018-11-12 11:15:07 -0500

    updated names

---
+++ client/src/components/Comment/Comment.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './Comment.css';
+
+export default class Comment extends Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    //link to HoloChain
+    alert('The value is: ' + this.input.value);
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="main-container">
+        <form onSubmit={this.handleSubmit}>
+        <label>
+          Add Comment:
+          <input className="comment" type="text" placeholder="Add Comment" ref={(input) => this.input = input} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+        </div>
+        <textarea name="" id="" cols="90" rows="30"></textarea>
+      </React.Fragment>
+    );
+  }
+}
--- client/src/components/TaskBox/TaskBox.js
+++ client/src/components/TaskBox/TaskBox.js
@@ -1,34 +1,12 @@
 import React, { Component } from 'react';
-import { Grid } from 'semantic-ui-react';
 import './TaskBox.css';
 
 export default class TaskBox extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <div className="box">
-          <Grid>
-            <Grid.Row>
-              <Grid.Column width={16}>
-                <p style={{textAlign:"center"}}>
-                  Given an Array and an Example-Array to sort to, write a function that sorts the
-                  Array following the Example-Array. Assume Example Array catalogs all elements
-                  possibly seen in the input Array. 
-                  <br />
-                  However, the input Array does not necessarily
-                  have to have all elements seen in the Example.
-                  <br />
-                  Example: Arr: [1,3,4,4,4,4,5]
-                  <br />
-                  Example Arr: [4,1,2,3,5]
-                  <br />
-                  Result: [4,4,4,4,1,3,5]
-                </p>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </div>
-      </React.Fragment>
-    );
-  }
+    render() {
+        return (
+            <div className="task-div">
+                {this.props.children}
+            </div>
+        );
+    }
 }
