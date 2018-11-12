a5a4eaf9ed95aac764c109826e7c6115f6c4ea45
Evan Simon Ross <evan.simon.ross@gmail.com>
2018-11-12 15:16:03 -0500

    Added redirect to task page that has just been created

--- client/src/pages/NewTask/NewTask.js
+++ client/src/pages/NewTask/NewTask.js
@@ -20,19 +20,26 @@ export default class Profile extends Component {
 
     getHash = () => {
         API.getUser()
-        .then(res => {
-            this.setState({ 
-                creator: res.data.hash,
-                maxPebbles: res.data.pebbles
-             });
-        }
-        ).catch(err =>
-            console.log(err)
-        );
+            .then(res => {
+                this.setState({
+                    creator: res.data.hash,
+                    maxPebbles: res.data.pebbles
+                });
+            }
+            ).catch(err =>
+                console.log(err)
+            );
     }
 
     createTask = task => {
-        
+        API.createTask(task)
+            .then(res => {
+                console.log(res);
+                window.location.pathname = `/task/${res.data}`
+            }
+            ).catch(err =>
+                console.log(err)
+            );
     }
 
     handleChange = event => {
@@ -46,21 +53,13 @@ export default class Profile extends Component {
 
     handleSubmit = e => {
         e.preventDefault();
-        const { pebbles, title, details, tags} = this.state
+        const { pebbles, title, details, tags } = this.state
 
         if (pebbles !== "" && title !== "" && details !== "" && tags !== "") {
             let tagarr = tags.split("#")
             tagarr = tagarr.filter((entry) => { return entry.trim() !== '' }).map(e => e.trim().replace(/[,]/g, ""))
-            const task = { pebbles, title, details, tags: tagarr}
-            console.log(task);
-            API.createTask(task)
-            .then(res => {
-                console.log(res);
-                //  window.location.pathname = `/marketplace`
-            }
-            ).catch(err =>
-                console.log(err)
-            );
+            const task = { pebbles, title, details, tags: tagarr }
+            this.createTask(task);
         } else {
             alert("Please enter all field")
         }
@@ -74,7 +73,7 @@ export default class Profile extends Component {
                     <Task>
                         <h2 className="new-task-title">New Task</h2>
                         <form>
-                        <div className="input-div" style={{ margin: "10px 0" }}>
+                            <div className="input-div" style={{ margin: "10px 0" }}>
                                 <div className="label-div" >
                                     <label>User Hash:</label>
                                 </div>
@@ -84,25 +83,25 @@ export default class Profile extends Component {
                                 <div className="label-div">
                                     <label>Pebbles:</label>
                                 </div>
-                                <input type="number" name="pebbles" placeholder={`(${this.state.maxPebbles} max)`} onChange={this.handleChange} style={{ width: "100px" }} value={this.state.pebbles} required/>
+                                <input type="number" name="pebbles" placeholder={`(${this.state.maxPebbles} max)`} onChange={this.handleChange} style={{ width: "100px" }} value={this.state.pebbles} required />
                             </div>
                             <div className="input-div">
                                 <div className="label-div">
                                     <label>Title:</label>
                                 </div>
-                                <input name="title" onChange={this.handleChange} placeholder='Enter short description' value={this.state.title} required/>
+                                <input name="title" onChange={this.handleChange} placeholder='Enter short description' value={this.state.title} required />
                             </div>
                             <div className="input-div">
                                 <div className="label-div">
                                     <label>Tags:</label>
                                 </div>
-                                <input  name="tags" onChange={this.handleChange} placeholder='Tags enter # (ex. #HTML #JS #CSS)' value={this.state.tags} required/> {/*onKeyPress={this.handleKeyPress}*/}
+                                <input name="tags" onChange={this.handleChange} placeholder='Tags enter # (ex. #HTML #JS #CSS)' value={this.state.tags} required /> {/*onKeyPress={this.handleKeyPress}*/}
                             </div>
                             <div className="input-div">
                                 <div className="label-div textarea-label">
                                     <label>Details:</label>
                                 </div>
-                                <textarea  name="details" onChange={this.handleChange} placeholder='Please enter a detail description of what is needed' value={this.state.details} required/>
+                                <textarea name="details" onChange={this.handleChange} placeholder='Please enter a detail description of what is needed' value={this.state.details} required />
                             </div>
                             <button className="submit-new-task" onClick={this.handleSubmit} >Submit</button>
                         </form>
