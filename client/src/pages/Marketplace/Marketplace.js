8469e664a7767d0f0d2234ba29dac0e0026df05b
sandynism <31261960+Sandynism@users.noreply.github.com>
2018-11-13 22:40:58 -0500

    added transaction data, fixed minor date, time

--- client/src/pages/Marketplace/Marketplace.js
+++ client/src/pages/Marketplace/Marketplace.js
@@ -1,184 +1,205 @@
 import React, { Component } from 'react';
-import Navbar from "../../components/Navbar";
-import Container from "../../components/Container";
-import HoverBox from "../../components/HoverBox/HoverBox";
-import MarketInfo from "../../components/HoverBox/MarketInfo";
-import Card from '../../components/Card'
+import Navbar from '../../components/Navbar';
+import Container from '../../components/Container';
+import HoverBox from '../../components/HoverBox/HoverBox';
+import MarketInfo from '../../components/HoverBox/MarketInfo';
+import Card from '../../components/Card';
 import './Marketplace.css';
-import API from "../../utils/API";
+import API from '../../utils/API';
 
 export default class Marketplace extends Component {
-    state = {
-        activeCard: "card-id-0",
-        fullList: "",
-        dataList: [],
-        currentPage: 1,
-        perPage: 5
-    }
+  state = {
+    activeCard: 'card-id-0',
+    fullList: '',
+    dataList: [],
+    currentPage: 1,
+    perPage: 5
+  };
 
-    componentDidMount = () => {
-        this.getCards(); //get all the info in the database
-    }
+  componentDidMount = () => {
+    this.getCards(); //get all the info in the database
+  };
 
-    // Data retrieval
-    getCards = () => {
-        API.getTasks()
-            .then(res => {
-                if(res.data.length===0) { return; }
-                this.setState({ dataList: res.data.links, fullList: res.data.links });
-            }
-        )
-            .catch(err =>
-            console.log(err)
-        );
-    }
-
-    // Filter Functions
-    filterCreator = name => {
-        const { fullList } = this.state
-        const newArray = fullList.filter(cardinfo => cardinfo.Entry.creator.includes(name));
-        this.setState({ dataList: newArray })
-    }
-
-    filterDate = date => {
-        // RejEx needed because Js date concersion with "-" is a day behind
-        const startDate = new Date(date.start.replace(/-/g, '/'))
-        const endDate = new Date(date.end.replace(/-/g, '/'))
-
-        const { fullList } = this.state
-        const newArray = fullList.filter(cardinfo => {
-            let cdate = new Date(cardinfo.Entry.time)
-            return cdate >= startDate && cdate <= endDate;
-        });
-        this.setState({ dataList: newArray })
-    }
-
-    filterTag = tag => {
-        const { fullList } = this.state
-        const newArray = fullList.filter(cardinfo => cardinfo.Entry.tags.includes(tag));
-        this.setState({ dataList: newArray })
-    }
-
-    filterPebbles = pebbleCount => {
-        const { fullList } = this.state
-        const newArray = fullList.filter(cardinfo => cardinfo.Entry.pebbles === parseInt(pebbleCount))
-        this.setState({ dataList: newArray})
-    }
-
-    clearFilter = () => {
-        this.setState({dataList: this.state.fullList})
-    }
-
-
-    // Sort functions
-    sortDate = (order) => {
-        let data = this.state.dataList
-        if (order === "newest-oldest") {
-            data.sort((a,b) => {
-                return new Date(b.Entry.time).getTime() - new Date(a.Entry.time).getTime();
-            })
-            this.setState({dataList: data, activeCard: "card-id-0", currentPage: 1})
-        } else if (order === "oldest-newest") {
-            data.sort((a,b) => {
-                return new Date(a.Entry.time).getTime() - new Date(b.Entry.time).getTime();
-            })
-            this.setState({dataList: data, activeCard: "card-id-0", currentPage: 1})
+  // Data retrieval
+  getCards = () => {
+    API.getTasks()
+      .then(res => {
+        if (res.data.length === 0) {
+          return;
         }
-        
+        this.setState({ dataList: res.data.links, fullList: res.data.links });
+      })
+      .catch(err => console.log(err));
+  };
+
+  // Filter Functions
+  filterCreator = name => {
+    const { fullList } = this.state;
+    const newArray = fullList.filter(cardinfo => cardinfo.Entry.creator.includes(name));
+    this.setState({ dataList: newArray });
+  };
+
+  filterDate = date => {
+    // RejEx needed because Js date concersion with "-" is a day behind
+    const startDate = new Date(date.start.replace(/-/g, '/'));
+    const endDate = new Date(date.end.replace(/-/g, '/'));
+
+    const { fullList } = this.state;
+    const newArray = fullList.filter(cardinfo => {
+      let cdate = new Date(cardinfo.Entry.time);
+      return cdate >= startDate && cdate <= endDate;
+    });
+    this.setState({ dataList: newArray });
+  };
+
+  filterTag = tag => {
+    const { fullList } = this.state;
+    const newArray = fullList.filter(cardinfo => cardinfo.Entry.tags.includes(tag));
+    this.setState({ dataList: newArray });
+  };
+
+  filterPebbles = pebbleCount => {
+    const { fullList } = this.state;
+    const newArray = fullList.filter(cardinfo => cardinfo.Entry.pebbles === parseInt(pebbleCount));
+    this.setState({ dataList: newArray });
+  };
+
+  clearFilter = () => {
+    this.setState({ dataList: this.state.fullList });
+  };
+
+  // Sort functions
+  sortDate = order => {
+    let data = this.state.dataList;
+    if (order === 'newest-oldest') {
+      data.sort((a, b) => {
+        return new Date(b.Entry.time).getTime() - new Date(a.Entry.time).getTime();
+      });
+      this.setState({ dataList: data, activeCard: 'card-id-0', currentPage: 1 });
+    } else if (order === 'oldest-newest') {
+      data.sort((a, b) => {
+        return new Date(a.Entry.time).getTime() - new Date(b.Entry.time).getTime();
+      });
+      this.setState({ dataList: data, activeCard: 'card-id-0', currentPage: 1 });
+    }
+  };
+
+  sortTitle = order => {
+    let data = this.state.dataList;
+    // console.log(data);
+    if (order === 'a-z') {
+      data.sort((a, b) => {
+        var textA = a.Entry.title.toUpperCase();
+        var textB = b.Entry.title.toUpperCase();
+        return textA < textB ? -1 : textA > textB ? 1 : 0;
+      });
+      this.setState({ dataList: data, activeCard: 'card-id-0', currentPage: 1 });
+    } else if (order === 'z-a') {
+      data.sort((a, b) => {
+        var textA = a.Entry.title.toUpperCase();
+        var textB = b.Entry.title.toUpperCase();
+        return textB < textA ? -1 : textB > textA ? 1 : 0;
+      });
+      this.setState({ dataList: data, activeCard: 'card-id-0', currentPage: 1 });
+    }
+  };
+
+  sortPebbles = order => {
+    let data = this.state.dataList;
+    if (order === 'min-max') {
+      data.sort((a, b) => {
+        var min = a.Entry.pebbles;
+        var max = b.Entry.pebbles;
+        return min < max ? -1 : min > max ? 1 : 0;
+      });
+      this.setState({ dataList: data, activeCard: 'card-id-0', currentPage: 1 });
+    } else if (order === 'max-min') {
+      data.sort((a, b) => {
+        var min = a.Entry.pebbles;
+        var max = b.Entry.pebbles;
+        return max < min ? -1 : max > min ? 1 : 0;
+      });
+      this.setState({ dataList: data, activeCard: 'card-id-0', currentPage: 1 });
+    }
+  };
+
+  // changes state active card to card that is clicked
+  handleCardClick = e => {
+    if (e.target.id.startsWith('card-id-')) {
+      this.setState({ activeCard: e.target.id });
+    }
+  };
+
+  // changes the info on card depending on which pagination btn is clicked
+  handlePagination = e =>
+    this.setState({ currentPage: Number(e.target.id), activeCard: 'card-id-0' });
+
+  render() {
+    const focus = 'left';
+
+    // Logic for getting the current card data
+    const { dataList, currentPage, perPage } = this.state;
+    const indexOfLast = currentPage * perPage;
+    const indexOfFirst = indexOfLast - perPage;
+    const currentList = dataList.slice(indexOfFirst, indexOfLast);
+
+    // Logic for displaying page numbers
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(dataList.length / perPage); i++) {
+      pageNumbers.push(i);
     }
 
-    sortTitle = (order) => {
-        let data = this.state.dataList
-        // console.log(data);
-        if (order === "a-z") {
-            data.sort((a,b) => {
-                var textA = a.Entry.title.toUpperCase();
-                var textB = b.Entry.title.toUpperCase();
-                return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
-            })
-            this.setState({dataList: data, activeCard: "card-id-0", currentPage: 1})
-        } else if (order === "z-a") {
-            data.sort((a,b) => {
-                var textA = a.Entry.title.toUpperCase();
-                var textB = b.Entry.title.toUpperCase();
-                return (textB < textA) ? -1 : (textB > textA) ? 1 : 0;
-            })
-            this.setState({dataList: data, activeCard: "card-id-0", currentPage: 1})
-        }
-    }
+    // renders the cards
+    const renderCards = currentList.map((card, i) => {
+      return (
+        <Card
+          key={i}
+          id={`card-id-${i}`}
+          hash={card.Hash}
+          activeCard={this.state.activeCard}
+          click={this.handleCardClick}
+          zIndex={20 - i}
+          info={card.Entry || card}
+        />
+      );
+    });
 
-    sortPebbles = (order) => {
-        let data = this.state.dataList
-        if (order === "min-max") {
-            data.sort((a,b) => {
-                var min = a.Entry.pebbles
-                var max = b.Entry.pebbles
-                return (min < max) ? -1 : (min > max) ? 1 : 0;
-            })
-            this.setState({dataList: data, activeCard: "card-id-0", currentPage: 1})
-        } else if (order === "max-min") {
-            data.sort((a,b) => {
-                var min = a.Entry.pebbles
-                var max = b.Entry.pebbles
-                return (max < min) ? -1 : (max > min) ? 1 : 0;
-            })
-            this.setState({dataList: data, activeCard: "card-id-0", currentPage: 1})
-        }
-    }
+    // renders the pagination btns
+    const renderPageNumbers = pageNumbers.map(number => (
+      <li key={number} id={number} onClick={this.handlePagination}>
+        {' '}
+        {number}
+      </li>
+    ));
 
-    // changes state active card to card that is clicked
-    handleCardClick = e => {
-        if (e.target.id.startsWith('card-id-')) {
-            this.setState({activeCard: e.target.id})
-        }
-    }
-
-    // changes the info on card depending on which pagination btn is clicked
-    handlePagination = (e) => this.setState({ currentPage: Number(e.target.id), activeCard: "card-id-0"})
-
-
-    render() {
-        const focus = "left"
-
-        // Logic for getting the current card data
-        const { dataList, currentPage, perPage } = this.state
-        const indexOfLast = currentPage * perPage
-        const indexOfFirst = indexOfLast - perPage
-        const currentList = dataList.slice(indexOfFirst, indexOfLast)
-
-        // Logic for displaying page numbers
-        const pageNumbers = [];
-        for (let i = 1; i <= Math.ceil(dataList.length / perPage); i++) {
-            pageNumbers.push(i);
-        }
-
-        // renders the cards
-        const renderCards = currentList.map((card, i) => {
-            return <Card key={i} id={`card-id-${i}`} hash={card.Hash} activeCard={this.state.activeCard} click={this.handleCardClick} zIndex={20-i} info={card.Entry || card}/>;
-        });
-
-        // renders the pagination btns
-        const renderPageNumbers = pageNumbers.map(number => <li key={number} id={number} onClick={this.handlePagination}> {number}</li>);
-
-        return (
-            <React.Fragment>
-                <Navbar page="Marketplace"/>
-                <HoverBox side={focus}>
-                    <MarketInfo prof={this.props.profSeed} filterCreator={this.filterCreator} filterDate={this.filterDate} filterTag={this.filterTag} filterPebbles={this.filterPebbles} clearFilter={this.clearFilter} sortDate={this.sortDate} sortTitle={this.sortTitle} sortPebbles={this.sortPebbles}/>
-                </HoverBox>
-                <Container padding={focus} bgcolor="rgb(32,32,32)">
-                    
-                    <div className="cardholder">
-                    <div className="new-task-div">
-                        <a href="/newtask"><button className="new-task-btn">+ New Task</button></a>
-                    </div>
-                        {renderCards}
-                    </div>
-                    <div className="pagination-div">
-                        {renderPageNumbers}
-                    </div>
-                </Container>
-            </React.Fragment>
-        );
-    }
+    return (
+      <React.Fragment>
+        <Navbar page="Marketplace" />
+        <HoverBox side={focus}>
+          <MarketInfo
+            prof={this.props.profSeed}
+            filterCreator={this.filterCreator}
+            filterDate={this.filterDate}
+            filterTag={this.filterTag}
+            filterPebbles={this.filterPebbles}
+            clearFilter={this.clearFilter}
+            sortDate={this.sortDate}
+            sortTitle={this.sortTitle}
+            sortPebbles={this.sortPebbles}
+          />
+        </HoverBox>
+        <Container padding={focus} bgcolor="rgb(32,32,32)">
+          <div className="cardholder">
+            <div className="new-task-div">
+              <a href="/newtask">
+                <button className="new-task-btn">+ New Task</button>
+              </a>
+            </div>
+            {renderCards}
+          </div>
+          <div className="pagination-div">{renderPageNumbers}</div>
+        </Container>
+      </React.Fragment>
+    );
+  }
 }
--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import Navbar from '../../components/Navbar';
 import Container from '../../components/Container';
 import HoverBox from '../../components/HoverBox/HoverBox';
-import ProfileInfo from '../../components/HoverBox/ProfileInfo';
+import Table from '../../components/Table';
+// import ProfileInfo from '../../components/HoverBox/ProfileInfo';
 import API from '../../utils/API';
 import './Profile.css';
 
@@ -17,23 +18,25 @@ export default class Profile extends Component {
 
   componentDidMount = () => {
     this.getHash();
+    this.getTransactions();
+    // this.sortWithdrawals()
   };
 
   getHash = () => {
     API.getUser()
       .then(res => {
-        let {github} = res.data.userdata[0].Entry
+        let { github } = res.data.userdata[0].Entry;
         this.setState({
           creator: github || res.data.hash,
           pebbles: res.data.pebbles
         });
-        if(github){
+        if (github) {
           API.getGithub(github)
             .then(res => {
               console.log(res);
               this.setState({
                 avatar: res.data.avatar_url
-              })
+              });
             })
             .catch(err => console.log(err));
         }
@@ -42,17 +45,30 @@ export default class Profile extends Component {
   };
 
   getTransactions = () => {
-    API.getTransactions().then(res => {
+    API.getTransactionHistory().then(res => {
+      console.log(res);
       this.setState({
         withdrawals: res.data.withdrawals,
         deposits: res.data.deposits
       });
+      // console.log(res.data.withdrawals)
+      // console.log(res.data.deposits)
     });
   };
 
+  // sortWithdrawals = () => {
+  //   let data = this.state.withdrawals;
+  //   data.sort((a, b) => {
+  //     return new Date(b.Entry.time).getTime() - new Date(a.Entry.time).getTime();
+  //   });
+  //   this.setState({ withdrawals: data });
+  //   console.log(data);
+  // };
+
   render() {
     const focus = 'left';
-
+    let { withdrawals, deposits } = this.state;
+    let data = withdrawals.concat(deposits)
     return (
       <React.Fragment>
         <Navbar page="Profile" />
@@ -60,39 +76,25 @@ export default class Profile extends Component {
           {/* <ProfileInfo prof={this.props.profSeed}/> */}
           <div className="input-div" style={{ margin: '10px 0' }}>
             <div className="label">
-              <label>Hi Code Penguin User</label>
+              <label>Hello, </label>
             </div>
-            <span className="span-user">{this.state.creator}</span>
-            <img width="150px" src={this.state.avatar} alt={this.state.creator}/>
-            <span className="span-pebbles">Pebble Count {this.state.pebbles}</span>
+            <span className="span-user">{this.state.creator.toUpperCase()}</span>
+            {/* <span className="span-user">QmWtWoTu6qeSkiSBW7F7sKnrEYowfunci5BLHzpZDuxTjy</span> */}
+            <img
+              className="avatar"
+              width="150px"
+              src={this.state.avatar}
+              alt={this.state.creator}
+            />
+            <span className="span-pebbles">Today's Pebble Count {this.state.pebbles}</span>
           </div>
         </HoverBox>
         <Container padding={focus} bgcolor="rgb(32,32,32)">
           <div className="div-404">
             <h2>Pebble Transaction History</h2>
+            <Table data={data} />
 
-            <table>
-              <tr>
-                <th>Date</th>
-                <th>Task Name</th>
-                <th>Pebbles</th>
-              </tr>
-              <tr>
-                <td>10/31/18</td>
-                <td>How to JavaScript</td>
-                <td>+25</td>
-              </tr>
-              <tr>
-                <td>11/2/18</td>
-                <td>Learn React</td>
-                <td>+100</td>
-              </tr>
-              <tr>
-                <td>12/1/18</td>
-                <td>Graduation</td>
-                <td>+200</td>
-              </tr>
-            </table>
+          
 
           </div>
         </Container>
@@ -100,3 +102,44 @@ export default class Profile extends Component {
     );
   }
 }
+
+// res.data.deposits/withdrawals.Entry.time/pebbles/origin
+// {
+//   "deposits": [
+//     {
+//       "Entry": {
+//         "destination": "QmXNJrCDFCpqFVRcWQ3Tr6uZ7N6GZ5ssQfKcMUzqQTiJaX",
+//         "origin": "QmTHhcNysEfNUyAczp9j9ACJW1jMho4yYnEzUinAMSqFg6",
+//         "pebbles": 1000,
+//         "time": 1542075242085
+//       },
+//       "EntryType": "transaction",
+//       "Hash": "QmbAa5qeHUFRJ49PcnwGWj8px7EbhFyUrAaNaw31cq4dHd",
+//       "Source": "QmXNJrCDFCpqFVRcWQ3Tr6uZ7N6GZ5ssQfKcMUzqQTiJaX"
+//     }
+//   ],
+//   "withdrawals": [
+//     {
+//       "Entry": {
+//         "destination": "QmdgL2Vcuyg9aoCp5E1f8pTq9W11YWkBp2pRT4resVwjsU",
+//         "origin": "QmXNJrCDFCpqFVRcWQ3Tr6uZ7N6GZ5ssQfKcMUzqQTiJaX",
+//         "pebbles": 25,
+//         "time": 1542075242136
+//       },
+//       "EntryType": "transaction",
+//       "Hash": "QmWZJsc9FhvmKJEjnXo3ro1LQ7oQHEew4MapuCwfXgPc2n",
+//       "Source": "QmXNJrCDFCpqFVRcWQ3Tr6uZ7N6GZ5ssQfKcMUzqQTiJaX"
+//     },
+//     {
+//       "Entry": {
+//         "destination": "QmbmgjGku2sQRtKF849ZtWWfhFHhacjracUMPU9AWVauyi",
+//         "origin": "QmXNJrCDFCpqFVRcWQ3Tr6uZ7N6GZ5ssQfKcMUzqQTiJaX",
+//         "pebbles": 200,
+//         "time": 1542075242244
+//       },
+//       "EntryType": "transaction",
+//       "Hash": "QmRbPRJYK2aQq949BHohRxLy2fXuzQPViUEEDnHX3iwNqy",
+//       "Source": "QmXNJrCDFCpqFVRcWQ3Tr6uZ7N6GZ5ssQfKcMUzqQTiJaX"
+//     }
+//   ]
+// }
