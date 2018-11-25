353d86ef0835005073dae428e6f7a8b80dbcd98e
KatHe <he.katherine321@gmail.com>
2018-11-25 00:46:13 -0500

    cant figure out login

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,45 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+import Login from "./pages/Login";
 import Landing from "./pages/Landing";
 import Profile from "./pages/Profile";
 import Marketplace from "./pages/Marketplace";
 import Task from "./pages/Task";
 import NewTask from "./pages/NewTask"
 import NoMatch from "./pages/NoMatch";
-import profSeed from "./profSeed.json"
-import cardSeed from "./cardSeed.json"
+// import profSeed from "./profSeed.json"
+// import cardSeed from "./cardSeed.json"
 import './App.css';
 
 export default class App extends Component {
+    state = {
+        user: ""
+    }
+
+    login = user => {
+        this.setState({ user: user })
+    }
+
+    logout = () => {
+        this.setState({ user: "" })
+    }
+
     render() {
+        console.log("user:", this.state.user);
+        
         return (
             <Router>
                 <Switch>
-                    <Route exact path="/" render={() => <Marketplace profSeed={profSeed} cardSeed={cardSeed}/>} />
+                    <Route exact path="/" render={() => <Redirect to="/login" /> } />
                     <Route path="/landing" component={Landing} />
-                    <Route path="/profile" render={() => <Profile profSeed={profSeed} />} />
-                    <Route path="/newtask" render={() => <NewTask profSeed={profSeed} />} />
-                    <Route path="/task/:hash" component={Task}/>
+                    <Route path="/profile" render={() => <Profile /> } />
+                    <Route path="/marketplace" component={Marketplace} />
+                    <Route path="/newtask" render={() => <NewTask />} />
+                    <Route path="/login" render={() => <Login getUser={this.login} />} />
+                    <Route path="/task/:hash" component={Task} />
                     <Route component={NoMatch} />
                 </Switch>
             </Router>
-        );
+        )
     }
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+// import Test from './test';
 import * as serviceWorker from './serviceWorker';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<Test />, document.getElementById('root'));
 
 serviceWorker.unregister();
